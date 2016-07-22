e2de75bffb3dec7f9a6b7e76eaaf75aea70949ad
Necip Yildiran <fazilyildiran@gmail.com>
2016-07-22 10:55:18 +0300

    NEWEST THE BEST VERSIONS

--- src/main/java/tr/com/srdc/cda2fhir/DataTypesTransformer.java
+++ src/main/java/tr/com/srdc/cda2fhir/DataTypesTransformer.java
@@ -6,24 +6,52 @@ import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
 import ca.uhn.fhir.model.dstu2.composite.QuantityDt;
 import ca.uhn.fhir.model.dstu2.composite.RangeDt;
 import ca.uhn.fhir.model.dstu2.composite.RatioDt;
+import ca.uhn.fhir.model.primitive.BooleanDt;
+import ca.uhn.fhir.model.primitive.DateDt;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
+import ca.uhn.fhir.model.primitive.DecimalDt;
+import ca.uhn.fhir.model.primitive.StringDt;
+import ca.uhn.fhir.model.primitive.UriDt;
 
+import org.openhealthtools.mdht.uml.hl7.datatypes.BL;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CD;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CV;
 import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_PQ;
 import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_TS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.TS;
+import org.openhealthtools.mdht.uml.hl7.datatypes.URL;
 import org.openhealthtools.mdht.uml.hl7.datatypes.PQ;
+import org.openhealthtools.mdht.uml.hl7.datatypes.REAL;
 import org.openhealthtools.mdht.uml.hl7.datatypes.RTO;
+import org.openhealthtools.mdht.uml.hl7.datatypes.ST;
 
 /**
  * Created by mustafa on 7/21/2016.
  */
 public interface DataTypesTransformer {
 
+	/* MATURITY LEVELS
+	 * 0: not ready to test
+	 * 1: needs testing
+	 * 2: tested
+	 * 3: finalized
+	 */
+	
     CodingDt CV2Coding(CV cv);
 
-    CodeableConceptDt CD2CodeableConcept(CD cd);
+    CodeableConceptDt CD2CodeableConcept(CD cd); /* Maturity Level: 0 */
+    
+    BooleanDt BL2Boolean(BL bl); /* Maturity Level: 1 */
+    
+    DateDt TS2Date(TS ts); /* Maturity Level: 1 */
+    
+    DecimalDt REAL2Decimal(REAL real); /* Maturity Level: 1 */
+    
+    StringDt ST2String(ST st); /* Maturity Level: 1 */
+    
+    UriDt URL2Uri(URL url); /* Maturity Level: 1 */
+    
+//    RatioDt RTO2Ratio(RTO rto); /* QuantityDt is required to implement this method */
     
     PeriodDt IVL_TS2Period(IVL_TS ivlts);
     
@@ -32,9 +60,5 @@ public interface DataTypesTransformer {
     QuantityDt PQ2Quantity(PQ pq);
    
     RangeDt IVL_PQ2Range(IVL_PQ ivlpq);
-    
-    RatioDt RTO2Ratio(RTO rto);
-    
-   
 
 }
--- src/main/java/tr/com/srdc/cda2fhir/impl/DataTypesTransformerImpl.java
+++ src/main/java/tr/com/srdc/cda2fhir/impl/DataTypesTransformerImpl.java
@@ -8,7 +8,12 @@ import ca.uhn.fhir.model.dstu2.composite.QuantityDt;
 import ca.uhn.fhir.model.dstu2.composite.RangeDt;
 import ca.uhn.fhir.model.dstu2.composite.RatioDt;
 import ca.uhn.fhir.model.dstu2.composite.SimpleQuantityDt;
+import ca.uhn.fhir.model.primitive.BooleanDt;
+import ca.uhn.fhir.model.primitive.DateDt;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
+import ca.uhn.fhir.model.primitive.DecimalDt;
+import ca.uhn.fhir.model.primitive.StringDt;
+import ca.uhn.fhir.model.primitive.UriDt;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
@@ -25,6 +30,7 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xml.type.SimpleAnyType;
+import org.openhealthtools.mdht.uml.hl7.datatypes.BL;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CD;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CV;
 import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_PQ;
@@ -32,8 +38,11 @@ import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_TS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.IVXB_TS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.PQ;
 import org.openhealthtools.mdht.uml.hl7.datatypes.PQR;
+import org.openhealthtools.mdht.uml.hl7.datatypes.REAL;
 import org.openhealthtools.mdht.uml.hl7.datatypes.RTO;
+import org.openhealthtools.mdht.uml.hl7.datatypes.ST;
 import org.openhealthtools.mdht.uml.hl7.datatypes.TS;
+import org.openhealthtools.mdht.uml.hl7.datatypes.URL;
 import org.openhealthtools.mdht.uml.hl7.vocab.NullFlavor;
 import org.openhealthtools.mdht.uml.hl7.vocab.SetOperator;
 
@@ -54,10 +63,93 @@ public class DataTypesTransformerImpl implements DataTypesTransformer {
     }
 
     public CodeableConceptDt CD2CodeableConcept(CD cd) {
-    	CodeableConceptDt codeableConceptDt = new CodeableConceptDt();
-    	
-    	return null;
+        return null;
     }
+    
+//    public Base64BinaryDt BIN2Base64Binary(BIN bin){
+//    	return bin.isSetNullFlavor() ? null :  SOMETHING REASONABLE;
+//    }
+    
+    public BooleanDt BL2Boolean(BL bl){
+     	return bl.isSetNullFlavor() ? null : new BooleanDt(bl.getValue());
+    }
+    public DateDt TS2Date(TS ts){
+    	if( ts.isSetNullFlavor() ) return null;
+    	else {
+    		DateDt resultDateDt = new DateDt();
+    		int lengthOfTheDateString = ts.getValue().length();
+    		String dateString = ts.getValue();
+    		boolean isPrecisionSet = false;
+    		
+    		switch(lengthOfTheDateString){
+    		/* The cases where the length of the string is 10, 7 or 4 is for the date of forms yyyy-mm-dd, yyyy-mm and yyyy
+    		 * The cases will leave the switch block at case 4 (by break). 
+    		 * If the date is of another form (yyyymmdd or yyyymm  ), then the following cases will apply (case 8,6) 
+    		 * */ 
+    		case 10: /* yyyy-mm-dd */
+    			if( !isPrecisionSet ) {
+    				/* If the precision hasn't set yet, set it and set isPrecisionSet to be true. */
+    				resultDateDt.setPrecision(TemporalPrecisionEnum.DAY);
+    				isPrecisionSet = true; 
+    			}
+    			resultDateDt.setDay( Integer.parseInt(dateString.substring(8,10)) );
+    		case 7: /* yyyy-mm */
+    			if( !isPrecisionSet ) {
+    				resultDateDt.setPrecision(TemporalPrecisionEnum.MONTH);
+    				isPrecisionSet = true; 
+    			}
+    			resultDateDt.setMonth( Integer.parseInt(dateString.substring(5,7)) );
+    		case 4: /* yyyy */
+    			if( !isPrecisionSet ) {
+    				resultDateDt.setPrecision(TemporalPrecisionEnum.YEAR);
+    				isPrecisionSet = true; 
+    			}
+    			resultDateDt.setYear( Integer.parseInt(dateString.substring(0,4)) );
+    			break;
+    		case 8: /* yyyymmdd */
+    			if( !isPrecisionSet ) {
+    				resultDateDt.setPrecision(TemporalPrecisionEnum.DAY);
+    				isPrecisionSet = true; 
+    			}
+    			resultDateDt.setDay( Integer.parseInt(dateString.substring(7,9)) );
+    			
+    		case 6: /* yyyymm */
+    			if( !isPrecisionSet ) {
+    				resultDateDt.setPrecision(TemporalPrecisionEnum.MONTH);
+    				isPrecisionSet = true; 
+    			}
+    			resultDateDt.setMonth( Integer.parseInt(dateString.substring(4,6)) );
+    			/* The case where the date is of form yyyy is covered in case 4.
+    			 * Therefore, we just set the year in one line for the cases yyyymm and yyyymmdd 
+    			 * */
+    			resultDateDt.setYear( Integer.parseInt(dateString.substring(0,4)) );
+    		}
+    	return resultDateDt;
+    	}
+    }
+    
+    public DecimalDt REAL2Decimal(REAL real){
+    	return real.isSetNullFlavor() ? null : new DecimalDt(real.getValue());
+    }
+    
+    public StringDt ST2String(ST st){
+    	return st.isSetNullFlavor() ? null : new StringDt(st.getText());
+    }
+    
+    public UriDt URL2Uri(URL url){
+    	return url.isSetNullFlavor() ? null : new UriDt(url.getValue());
+    }
+    /* QuantityDt is required. 
+     * When ready, please fill in the gaps: <<QuantityDt>> */
+//    public RatioDt RTO2Ratio(RTO rto){
+//    	if( rto.isNullFlavorDefined() ) return null;
+//    	else{
+//    		RatioDt myRatioDt = new RatioDt();
+//    		myRatioDt.setNumerator( <<QuantityDt>> );
+//    		myRatioDt.setDenominator( <<QuantityDt>> );
+//    		return myRatioDt;
+//    	}
+//    }
 
 	public PeriodDt IVL_TS2Period(IVL_TS ivlts) {
 		
