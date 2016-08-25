451016b0493515a6b8f4f64f5112b9c4281e9b75
Mustafa Yuksel <msfyuksel@gmail.com>
2016-08-25 11:16:32 +0300

    completion of cda transformer comments

--- src/main/java/tr/com/srdc/cda2fhir/CCDATransformer.java
+++ src/main/java/tr/com/srdc/cda2fhir/CCDATransformer.java
@@ -11,7 +11,7 @@ import tr.com.srdc.cda2fhir.util.IdGeneratorEnum;
 public interface CCDATransformer {
 
     /**
-     * Sets the resource id generator format, which is either a COUNTER or UUID
+     * Sets the resource id generator format, which is either an incremental COUNTER or UUID
      * @param idGen The id generator enumeration to be set
      */
     void setIdGenerator(IdGeneratorEnum idGen);
@@ -22,7 +22,16 @@ public interface CCDATransformer {
      */
     String getUniqueId();
 
+    /**
+     * Returns a ResourceReferenceDt for the patient of the CCD document
+     * @return A ResourceReferenceDt that references the patient (i.e. recordTarget/patientRole) of the document
+     */
     ResourceReferenceDt getPatientRef();
 
+    /**
+     * Transforms a Consolidated CDA (C-CDA) 2.1 Continuity of Care Document (CCD) instance to a Bundle of corresponding FHIR resources
+     * @param ccd A Consolidated CDA (C-CDA) 2.1 Continuity of Care Document (CCD) instance to be transformed
+     * @return A FHIR Bundle that contains a Composition corresponding to the CCD document and all other resources that are referenced within the Composition.
+     */
     Bundle transformCCD(ContinuityOfCareDocument ccd);
 }
