b7a56c24610cbc8a8f9bbdcb7f0146b40ac75b0f
Afsin Ustundag <afsin@amida.com>
2019-02-13 12:00:50 -0500

    reimplement Condition category and add unit test

--- src/test/java/tr/com/srdc/cda2fhir/ProblemConcernActTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/ProblemConcernActTest.java
@@ -1,11 +1,13 @@
 package tr.com.srdc.cda2fhir;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.Condition;
 import org.hl7.fhir.dstu3.model.Condition.ConditionClinicalStatus;
 import org.hl7.fhir.dstu3.model.Condition.ConditionVerificationStatus;
@@ -25,6 +27,7 @@ import org.openhealthtools.mdht.uml.hl7.datatypes.CE;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
 import org.openhealthtools.mdht.uml.hl7.datatypes.II;
+import org.openhealthtools.mdht.uml.hl7.datatypes.impl.CDImpl;
 import org.openhealthtools.mdht.uml.hl7.datatypes.impl.DatatypesFactoryImpl;
 import org.openhealthtools.mdht.uml.hl7.vocab.NullFlavor;
 import org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship;
@@ -80,15 +83,62 @@ public class ProblemConcernActTest {
 		return problemStatus;
 	}
 	
-	@Test
-	public void testProblemObservationProblemStatus() throws Exception {
+	private static ProblemConcernActImpl createProblemConcernAct() {
 		ProblemConcernActImpl act = (ProblemConcernActImpl) cdaObjFactory.createProblemConcernAct();
 		ProblemObservationImpl observation = (ProblemObservationImpl) cdaObjFactory.createProblemObservation();
 		act.addObservation(observation);
 		act.getEntryRelationships().stream()
 			.filter(r -> (r.getObservation() == observation))
 			.forEach(r -> r.setTypeCode(x_ActRelationshipEntryRelationship.SUBJ));
+		return act;
+	}
 
+	static private void verifyCoding(Coding coding, String code, String displayName, String system) {
+		Assert.assertEquals("Unexpected Coding code", code, coding.getCode());
+		Assert.assertEquals("Unexpected Coding display name", displayName, coding.getDisplay());
+		Assert.assertEquals("Unexpected Coding system", system, coding.getSystem());
+	}
+	
+	@Test
+	public void testProblemObservationCode() throws Exception {
+		ProblemConcernActImpl act = createProblemConcernAct();
+		ProblemObservationImpl observation =  (ProblemObservationImpl) act.getEntryRelationships().get(0).getObservation();
+		DiagnosticChain dxChain = new BasicDiagnostic();
+
+		String code = "404684003"; // From CCDA Specification
+		String displayName = "Finding";		
+		CDImpl cd = (CDImpl) cdaTypeFactory.createCD(code, "2.16.840.1.113883.6.96", "SNOMED CT", displayName);
+		observation.setCode(cd);
+		
+		Boolean validation = act.validateProblemConcernActProblemObservation(dxChain, null);
+		Assert.assertTrue("Invalid Problem Concern Act in Test", validation);
+
+		Bundle bundle = rt.tProblemConcernAct2Condition(act);
+		Condition condition = BundleUtil.findOneResource(bundle, Condition.class);
+		List<Coding> category = condition.getCategory().get(0).getCoding();
+		Assert.assertEquals("Unexpected number of category codings", 1, category.size());
+		verifyCoding(category.get(0), code, displayName, "http://snomed.info/sct");
+		
+		String translationCode = "75321-0"; // From CCDA Specification
+		String translationDisplayName = "Clinical finding HL7.CCDAR2";
+		CD translationCd = cdaTypeFactory.createCD(translationCode, "2.16.840.1.113883.6.1", "LOINC", translationDisplayName);
+		cd.getTranslations().add(translationCd);
+
+		Boolean validation2 = act.validateProblemConcernActProblemObservation(dxChain, null);
+		Assert.assertTrue("Invalid Problem Concern Act in Test", validation2);
+
+		Bundle bundle2 = rt.tProblemConcernAct2Condition(act);
+		Condition condition2 = BundleUtil.findOneResource(bundle2, Condition.class);
+		List<Coding> category2 = condition2.getCategory().get(0).getCoding();
+		Assert.assertEquals("Unexpected number of category codings", 2, category2.size());
+		verifyCoding(category2.get(0), code, displayName, "http://snomed.info/sct");
+		verifyCoding(category2.get(1), translationCode, translationDisplayName, "http://loinc.org");
+	}
+		
+	@Test
+	public void testProblemObservationProblemStatus() throws Exception {
+		ProblemConcernActImpl act = createProblemConcernAct();
+		ProblemObservationImpl observation =  (ProblemObservationImpl) act.getEntryRelationships().get(0).getObservation();
 		
 		for (Map.Entry<String, Object> entry : clinicalStatusMap.entrySet()) {
 			String cdaProblemStatusCode = entry.getKey();
@@ -145,8 +195,7 @@ public class ProblemConcernActTest {
 		Boolean validationNF = act.validateProblemConcernActStatusCode(dxChain, null);
 		Assert.assertTrue("Invalid Problem Concern Act in Test", validationNF);
 		verifyConditionVerificationStatus(act, "unknown");
-		
-		
+			
 		for (Map.Entry<String, Object> entry : verificationStatusMap.entrySet()) {
 			String cdaStatusCode = entry.getKey();
 			String fhirStatus = (String) entry.getValue();
