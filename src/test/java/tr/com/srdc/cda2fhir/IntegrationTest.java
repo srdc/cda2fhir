d122738c030b0a01042febf47ce146b01c9a8b60
Stee <stp.staley@gmail.com>
2019-02-20 13:45:44 -0500

    removing further unused imports

--- src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
@@ -1,36 +1,23 @@
 package tr.com.srdc.cda2fhir;
 
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.hl7.fhir.dstu3.formats.IParser;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.dstu3.model.Bundle.BundleEntryResponseComponent;
 import org.hl7.fhir.dstu3.model.Bundle.BundleType;
 import org.hl7.fhir.dstu3.model.Bundle.BundleTypeEnumFactory;
-import org.hl7.fhir.dstu3.model.Medication;
 import org.hl7.fhir.dstu3.model.Patient;
-import org.hl7.fhir.dstu3.model.Reference;
-import org.hl7.fhir.dstu3.model.Resource;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.dstu3.model.ProcessResponse;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-//import org.testng.annotations.DataProvider;
 
 import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
@@ -41,15 +28,11 @@ import com.palantir.docker.compose.connection.waiting.HealthChecks;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.rest.client.api.IRestfulClient;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.ValidationResult;
-import tr.com.srdc.cda2fhir.conf.Config;
 import tr.com.srdc.cda2fhir.transform.CCDTransformerImpl;
-import tr.com.srdc.cda2fhir.transform.ICDATransformer;
 import tr.com.srdc.cda2fhir.util.FHIRUtil;
 import tr.com.srdc.cda2fhir.util.IdGeneratorEnum;
-import ca.uhn.fhir.parser.JsonParser;
 
 public class IntegrationTest{
 	static String hapiURL = "http://localhost:8080";
@@ -90,8 +73,6 @@ public class IntegrationTest{
 			
 	        FileInputStream fis = new FileInputStream("src/test/resources/" + sourceName);
 	        ClinicalDocument cda = CDAUtil.load(fis);
-	        
-	        BundleType bt = bundleTypeEnumFactory.fromCode("transaction");
 	        Bundle bundle = ccdTransformer.transformDocument(cda);
 	       
 	        return bundle;
