995d9863c4677fefdb42a231f63cc2a594ea1e9b
steven <steven.staley@amida.com>
2019-02-26 10:28:35 -0500

    restructuring file

--- src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
@@ -1,12 +1,6 @@
 package tr.com.srdc.cda2fhir;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
@@ -19,15 +13,12 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-
-import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.waiting.HealthChecks;
 
 import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.ValidationResult;
@@ -35,91 +26,76 @@ import tr.com.srdc.cda2fhir.transform.CCDTransformerImpl;
 import tr.com.srdc.cda2fhir.util.FHIRUtil;
 import tr.com.srdc.cda2fhir.util.IdGeneratorEnum;
 
-public class IntegrationTest{
+public class IntegrationTest {
 	static String hapiURL = "http://localhost:8080";
 	static String serverBase = hapiURL + "/baseDstu3";
 	static FhirContext ctx;
 	static IGenericClient client;
 	static CCDTransformerImpl ccdTransformer;
-	
+
 	@BeforeClass
-    public static void init() throws IOException {
-		
-        // Load MDHT CDA packages. Otherwise ContinuityOfCareDocument and similar documents will not be recognised.
-        // This has to be called before loading the document; otherwise will have no effect.
-        CDAUtil.loadPackages();
-        ctx = FhirContext.forDstu3();
-        client = ctx.newRestfulGenericClient(serverBase);
-        ccdTransformer = new CCDTransformerImpl(IdGeneratorEnum.COUNTER);
-    }
-	
+	public static void init() throws IOException {
+
+		// Load MDHT CDA packages. Otherwise ContinuityOfCareDocument and similar
+		// documents will not be recognised.
+		// This has to be called before loading the document; otherwise will have no
+		// effect.
+		CDAUtil.loadPackages();
+		ctx = FhirContext.forDstu3();
+		client = ctx.newRestfulGenericClient(serverBase);
+		ccdTransformer = new CCDTransformerImpl(IdGeneratorEnum.COUNTER);
+	}
+
 	@ClassRule
-    public static DockerComposeRule docker = DockerComposeRule.builder()
-            .file("src/test/resources/docker-compose.yaml")
-            .waitingForService("hapi", HealthChecks.toRespondOverHttp(8080, (port) -> port.inFormat(hapiURL)))
-            .build();
-	
+	public static DockerComposeRule docker = DockerComposeRule.builder().file("src/test/resources/docker-compose.yaml")
+			.waitingForService("hapi", HealthChecks.toRespondOverHttp(8080, (port) -> port.inFormat(hapiURL))).build();
 
 	@SuppressWarnings("unused")
 	private static void validate(Bundle bundle) {
 		FhirValidator validator = new FhirValidator(ctx);
-    	ValidationResult result = validator.validateWithResult(bundle);
-    	if(!result.isSuccessful()) {
-    		System.out.println(result.toString());
-    	}
-    	Assert.assertTrue(result.isSuccessful());
+		ValidationResult result = validator.validateWithResult(bundle);
+		if (!result.isSuccessful()) {
+			System.out.println(result.toString());
+		}
+		Assert.assertTrue(result.isSuccessful());
 	}
-	
-	
-    @Test
+
+	@Test
 	public void rakiaIntegration() throws Exception {
-    	String sourceName = "Cerner/Person-RAKIA_TEST_DOC00001 (1).XML";	
-    	// create transaction bundle from ccda bundle
-    	Bundle transactionBundle = ccdTransformer.transformDocument("src/test/resources/" + sourceName, BundleType.TRANSACTION, null);
-   
+		String sourceName = "Cerner/Person-RAKIA_TEST_DOC00001 (1).XML";
+		// create transaction bundle from ccda bundle
+		Bundle transactionBundle = ccdTransformer.transformDocument("src/test/resources/" + sourceName,
+				BundleType.TRANSACTION, null);
+
 		// currently doesn't validate. Potentially due to incorrect implementation
 		// of resourceProfileMap.
 		// TODO: Make valid.
 		// validate(transactionBundle);
-    	
-    	// print pre-post bundle
-    	FHIRUtil.printJSON(transactionBundle, "src/test/resources/output/rakia_bundle.json");
-    	
-    	// Send transaction bundle to server.
-    	Bundle resp = client.transaction().withBundle(transactionBundle).execute();
-    	
-    	for(BundleEntryComponent entry : resp.getEntry()) {
-    		BundleEntryResponseComponent entryResp =entry.getResponse();
-    		
-    		Assert.assertEquals("201 Created", entryResp.getStatus());
-    	}
-    	
-    	Bundle patientResults = (Bundle)client
-	    	      .search()
-	    	      .forResource(Patient.class)
-	    	      .prettyPrint()
-	    	      .execute();
-    	
-    	Bundle practitionerResults = (Bundle)client
-	    	      .search()
-	    	      .forResource(Practitioner.class)
-	    	      .prettyPrint()
-	    	      .execute();
-    	
-    	Bundle medicationResults = (Bundle)client
-	    	      .search()
-	    	      .forResource(Medication.class)
-	    	      .prettyPrint()
-	    	      .execute();
-    
-    	Assert.assertEquals(1, patientResults.getTotal());
-    	Assert.assertEquals(18, practitionerResults.getTotal());
-    	Assert.assertEquals(14, medicationResults.getTotal());
-    	
-    	// TODO: Test each entry exists in server via search
-    	
-	}
-    
 
-    
+		// print pre-post bundle
+		FHIRUtil.printJSON(transactionBundle, "src/test/resources/output/rakia_bundle.json");
+
+		// Send transaction bundle to server.
+		Bundle resp = client.transaction().withBundle(transactionBundle).execute();
+
+		for (BundleEntryComponent entry : resp.getEntry()) {
+			BundleEntryResponseComponent entryResp = entry.getResponse();
+
+			Assert.assertEquals("201 Created", entryResp.getStatus());
+		}
+
+		Bundle patientResults = (Bundle) client.search().forResource(Patient.class).prettyPrint().execute();
+
+		Bundle practitionerResults = (Bundle) client.search().forResource(Practitioner.class).prettyPrint().execute();
+
+		Bundle medicationResults = (Bundle) client.search().forResource(Medication.class).prettyPrint().execute();
+
+		Assert.assertEquals(1, patientResults.getTotal());
+		Assert.assertEquals(18, practitionerResults.getTotal());
+		Assert.assertEquals(14, medicationResults.getTotal());
+
+		// TODO: Test each entry exists in server via search
+
+	}
+
 }
