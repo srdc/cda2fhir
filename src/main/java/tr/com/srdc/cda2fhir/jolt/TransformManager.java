c37fe4e78dfc308e5545fc9ceaaa7da85b6fc0b9
Afsin Ustundag <afsin@amida.com>
2019-03-08 09:08:07 -0500

    move resource creation to templates, retire ValueSetTransform and EntryToReource

--- src/main/java/tr/com/srdc/cda2fhir/jolt/TransformManager.java
+++ src/main/java/tr/com/srdc/cda2fhir/jolt/TransformManager.java
@@ -2,7 +2,6 @@ package tr.com.srdc.cda2fhir.jolt;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,26 +15,15 @@ public class TransformManager {
 		return context;
 	}
 
-	private static List<Object> getTopEntryTemplate() {
-        Map<String, Object> template = new LinkedHashMap<String, Object>();
-        template.put("operation", "tr.com.srdc.cda2fhir.jolt.EntryToResource");
-        List<Object> chainrSpec = new ArrayList<Object>();
-		chainrSpec.add(template);
-		return chainrSpec;
-	}
-	
 	public static Object transformEntryInFile(String cdaName, String filepath) {
-		//String specpath = String.format("src/test/resources/jolt/entry/%s.json", cdaName);		
-		//List<Object> chainrSpec = JsonUtils.filepathToList(specpath);
+		String specpath = String.format("src/test/resources/jolt/entry/%s.json", cdaName);		
+		List<Object> chainrSpec = JsonUtils.filepathToList(specpath);
 		
-		List<Object> chainrSpec = getTopEntryTemplate();
         Chainr chainr = Chainr.fromSpec(chainrSpec);
         Object input = JsonUtils.filepathToObject(filepath);
-        Map<String, Object> extendedInput = new LinkedHashMap<String, Object>();
-        extendedInput.put("--" + cdaName, input);
  
         Map<String, Object> context = getInitialContext();
-        chainr.transform(extendedInput, context);		
+        chainr.transform(input, context);		
         Object result = context.get("Resources");
         return result;
 	}
