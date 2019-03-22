c4db9ad8314e9d49332147d1d0148774101b3654
Afsin Ustundag <afsin@amida.com>
2019-03-22 07:30:40 -0400

    consolidate jolt data types tests into one start TEL test

--- src/test/java/tr/com/srdc/cda2fhir/jolt/DataTypeTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/jolt/DataTypeTest.java
@@ -13,13 +13,14 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import com.bazaarvoice.jolt.Chainr;
 import com.bazaarvoice.jolt.JsonUtils;
 
-public class CDTest {
-	@Test
-	public void testTemplate() throws Exception {
-		List<Object> chainrSpecJSON = JsonUtils.filepathToList("src/test/resources/jolt/data-type/CD.json");
+public class DataTypeTest {
+	private static void runDataTypeTests(String dataType) throws Exception {
+		String templatePath = String.format("src/test/resources/jolt/data-type/%s.json", dataType);
+		List<Object> chainrSpecJSON = JsonUtils.filepathToList(templatePath);
 		Chainr chainr = Chainr.fromSpec(chainrSpecJSON);
 
-		File file = new File("src/test/resources/jolt-verify/data-type/CD.json");
+		String testCasesPath = String.format("src/test/resources/jolt-verify/data-type/%s.json", dataType);
+		File file = new File(testCasesPath);
 		String content = FileUtils.readFileToString(file, Charset.defaultCharset());
 		JSONArray testCases = new JSONArray(content);
 		for (int index = 0; index < testCases.length(); ++index) {
@@ -33,7 +34,22 @@ public class CDTest {
 
 			String actual = JsonUtils.toJsonString(actualObject);
 			String expected = expectedJSON.toString();
-			JSONAssert.assertEquals("Test Case " + index, expected, actual, false);
+			JSONAssert.assertEquals(dataType + " test case " + index, expected, actual, true);
 		}
+	}
+
+	@Test
+	public void testID() throws Exception {
+		runDataTypeTests("ID");
+	}
+
+	@Test
+	public void testCD() throws Exception {
+		runDataTypeTests("CD");
+	}
+
+	@Test
+	public void testTEL() throws Exception {
+		runDataTypeTests("TEL");
 	}
 }
