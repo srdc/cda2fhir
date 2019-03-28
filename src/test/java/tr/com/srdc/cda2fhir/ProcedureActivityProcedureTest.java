041da4471ca0e1cdc67781c9c6d0b76e94b7a9f9
Afsin Ustundag <afsin@amida.com>
2019-03-28 15:10:28 -0400

    remove duplicate testing

--- src/test/java/tr/com/srdc/cda2fhir/ProcedureActivityProcedureTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/ProcedureActivityProcedureTest.java
@@ -5,24 +5,19 @@ import java.util.Map;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.Procedure;
 import org.hl7.fhir.dstu3.model.Procedure.ProcedureStatus;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openhealthtools.mdht.uml.cda.EntryRelationship;
-import org.openhealthtools.mdht.uml.cda.consol.Indication;
 import org.openhealthtools.mdht.uml.cda.consol.ProcedureActivityProcedure;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CS;
-import org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship;
 
 import com.bazaarvoice.jolt.JsonUtils;
 
 import tr.com.srdc.cda2fhir.testutil.BundleUtil;
 import tr.com.srdc.cda2fhir.testutil.CDAFactories;
-import tr.com.srdc.cda2fhir.testutil.generator.IndicationGenerator;
 import tr.com.srdc.cda2fhir.testutil.generator.ProcedureActivityProcedureGenerator;
 import tr.com.srdc.cda2fhir.transform.ResourceTransformerImpl;
 import tr.com.srdc.cda2fhir.transform.entry.impl.EntryResult;
@@ -43,10 +38,13 @@ public class ProcedureActivityProcedureTest {
 	}
 
 	@Test
-	public void testPerformer() throws Exception {
+	public void testDefault() throws Exception {
 		ProcedureActivityProcedureGenerator generator = ProcedureActivityProcedureGenerator.getDefaultInstance();
 
 		ProcedureActivityProcedure pap = generator.generate(factories);
+
+		DiagnosticChain dxChain = new BasicDiagnostic();
+		pap.validateProcedureActivityProcedureIndication(dxChain, null);
 
 		BundleInfo bundleInfo = new BundleInfo(rt);
 		EntryResult entryResult = rt.tProcedure2Procedure(pap, bundleInfo);
@@ -89,32 +87,5 @@ public class ProcedureActivityProcedureTest {
 
 			verifyProcedureStatus(pap, fhirStatus);
 		}
-	}
-
-	@Test
-	public void testIndication() throws Exception {
-		ProcedureActivityProcedure pap = factories.consol.createProcedureActivityProcedure();
-
-		IndicationGenerator indicationGenerator = new IndicationGenerator();
-		Indication indication = indicationGenerator.generate(factories);
-
-		EntryRelationship entryRelationship = factories.base.createEntryRelationship();
-		entryRelationship.setTypeCode(x_ActRelationshipEntryRelationship.RSON);
-		entryRelationship.setObservation(indication);
-
-		pap.getEntryRelationships().add(entryRelationship);
-
-		DiagnosticChain dxChain = new BasicDiagnostic();
-		pap.validateProcedureActivityProcedureIndication(dxChain, null);
-
-		BundleInfo bundleInfo = new BundleInfo(rt);
-		EntryResult entryResult = rt.tProcedure2Procedure(pap, bundleInfo);
-		Bundle bundle = entryResult.getBundle();
-		Procedure procedure = BundleUtil.findOneResource(bundle, Procedure.class);
-		Assert.assertTrue("Expect a reason code", procedure.hasReasonCode());
-		Coding coding = procedure.getReasonCode().get(0).getCodingFirstRep();
-		Assert.assertEquals("Expect the default code", IndicationGenerator.DEFAULT_CODE_CODE, coding.getCode());
-		Assert.assertEquals("Expect the default display name", IndicationGenerator.DEFAULT_CODE_DISPLAYNAME,
-				coding.getDisplay());
 	}
 }
