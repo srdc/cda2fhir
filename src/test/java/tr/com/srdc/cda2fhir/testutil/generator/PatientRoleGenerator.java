84c3e3a84de90bdca52f36ba6797b02cf8bd6ef1
Afsin Ustundag <afsin@amida.com>
2019-04-05 06:47:03 -0400

    patient telecom and addressjolt implementation

--- src/test/java/tr/com/srdc/cda2fhir/testutil/generator/PatientRoleGenerator.java
+++ src/test/java/tr/com/srdc/cda2fhir/testutil/generator/PatientRoleGenerator.java
@@ -6,19 +6,33 @@ import java.util.List;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.junit.Assert;
 import org.openhealthtools.mdht.uml.cda.PatientRole;
+import org.openhealthtools.mdht.uml.hl7.datatypes.AD;
 import org.openhealthtools.mdht.uml.hl7.datatypes.II;
+import org.openhealthtools.mdht.uml.hl7.datatypes.TEL;
 
 import tr.com.srdc.cda2fhir.testutil.CDAFactories;
 
 public class PatientRoleGenerator {
 	private List<IDGenerator> idGenerators = new ArrayList<>();
+	private List<ADGenerator> addrGenerators = new ArrayList<>();
+	private List<TELGenerator> telecomGenerators = new ArrayList<>();
 
 	public PatientRole generate(CDAFactories factories) {
 		PatientRole pr = factories.base.createPatientRole();
 
-		idGenerators.forEach(idGenerator -> {
-			II ii = idGenerator.generate(factories);
+		idGenerators.forEach(g -> {
+			II ii = g.generate(factories);
 			pr.getIds().add(ii);
+		});
+
+		addrGenerators.forEach(g -> {
+			AD ad = g.generate(factories);
+			pr.getAddrs().add(ad);
+		});
+
+		telecomGenerators.forEach(g -> {
+			TEL tel = g.generate(factories);
+			pr.getTelecoms().add(tel);
 		});
 
 		return pr;
@@ -28,6 +42,8 @@ public class PatientRoleGenerator {
 		PatientRoleGenerator prg = new PatientRoleGenerator();
 
 		prg.idGenerators.add(IDGenerator.getNextInstance());
+		prg.addrGenerators.add(ADGenerator.getDefaultInstance());
+		prg.telecomGenerators.add(TELGenerator.getDefaultInstance());
 
 		return prg;
 	}
@@ -36,9 +52,19 @@ public class PatientRoleGenerator {
 		if (idGenerators.isEmpty()) {
 			Assert.assertTrue("No patient identifier", !patient.hasIdentifier());
 		} else {
-			for (int index = 0; index < idGenerators.size(); ++index) {
-				idGenerators.get(index).verify(patient.getIdentifier().get(index));
-			}
+			IDGenerator.verifyList(patient.getIdentifier(), idGenerators);
+		}
+
+		if (addrGenerators.isEmpty()) {
+			Assert.assertTrue("No patient address", !patient.hasAddress());
+		} else {
+			ADGenerator.verifyList(patient.getAddress(), addrGenerators);
+		}
+
+		if (telecomGenerators.isEmpty()) {
+			Assert.assertTrue("No patient telecom", !patient.hasTelecom());
+		} else {
+			TELGenerator.verifyList(patient.getTelecom(), telecomGenerators);
 		}
 	}
 }
