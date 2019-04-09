18106715ca68d112c5d4d49beb9386817e0f12b1
jmfadeley@gmail.com <jmfadeley@gmail.com>
2019-04-09 15:58:55 -0400

    Test writing and results.

--- src/test/java/tr/com/srdc/cda2fhir/jolt/report/UnmappedFieldTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/jolt/report/UnmappedFieldTest.java
@@ -3,6 +3,7 @@ package tr.com.srdc.cda2fhir.jolt.report;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,23 +12,34 @@ import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class UnmappedFieldTest {
 	final private static String INPUT_PATH = "src/test/resources/";
+	final private static String OUTPUT_PATH = System.getProperty("user.dir") + "/src/test/resources/unmapped/";
 	final private static String REPORT_PATH = "src/test/resources/gold/jolt-report/";
+	final private static DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.newInstance();
 	final private static XPath XPATH = XPathFactory.newInstance().newXPath();
 
 	// Returns the comparison list from the CSV.
-	private static List<List<String>> csvToList(String fileName) throws IOException {
+	public static List<List<String>> csvToList(String fileName) throws IOException {
 		File csv = new File(REPORT_PATH + fileName + ".csv");
 
 		List<List<String>> records = new ArrayList<>();
@@ -43,33 +55,57 @@ public class UnmappedFieldTest {
 	}
 
 	// Returns the original XML as a Document.
-	public static Document convertBody(String sourcePath)
+	public static Document convertFileToDocument(String sourcePath)
 			throws IOException, ParserConfigurationException, SAXException {
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+		DocumentBuilder dbBuilder = DOCUMENT_BUILDER_FACTORY.newDocumentBuilder();
 
 		return dbBuilder.parse(new File(sourcePath));
 	}
 
-	@Test
-	public void testAllergyConcernAct()
-			throws IOException, XPathExpressionException, ParserConfigurationException, SAXException {
-		List<List<String>> records = csvToList("AllergyConcernAct");
-		Document body = convertBody(INPUT_PATH + "C-CDA_R2-1_CCD.xml");
-		NodeList xPathDocument = (NodeList) XPATH.evaluate("//*[local-name()='act']", body, XPathConstants.NODESET);
-		// /ClinicalDocument/component/structuredBody/component/section/entry/act/effectiveTime
-		// //act/(* except effectiveTime except entryRelationship except statusCode.code
-		// except Practitioner)
-		for (int i = 0; i < xPathDocument.getLength(); i++) {
-			System.out.println(xPathDocument.item(i).getNodeName());
-		}
-//		for (int i = 0; i < xmlNodes.getLength(); i++) {
-//			System.out.println(xmlNodes.item(i).getNodeName());
-//		}
-		for (List<String> list : records) {
-			System.out.println(list.get(0));
+	public static void convertNodeListToFile(NodeList nodeList, String sourcePath) throws ParserConfigurationException,
+			TransformerFactoryConfigurationError, IOException, TransformerException {
+		Document xmlDocument = DOCUMENT_BUILDER_FACTORY.newDocumentBuilder().newDocument();
+		Element root = xmlDocument.createElement("root");
+		root.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		root.setAttribute("xmlns:sdtc", "urn:hl7-org:sdtc");
+		xmlDocument.appendChild(root);
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			root.appendChild(xmlDocument.importNode(nodeList.item(i), true));
 		}
 
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+		transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+		DOMSource source = new DOMSource(xmlDocument);
+		FileWriter writer = new FileWriter(sourcePath);
+		StreamResult result = new StreamResult(writer);
+		transformer.transform(source, result);
+	}
+
+	@Before
+	public void createUnmappedDirectory() {
+		File directory = new File(OUTPUT_PATH);
+		directory.mkdir();
+	}
+
+	@Test
+	public void testAllergyConcernAct() throws IOException, XPathExpressionException, ParserConfigurationException,
+			SAXException, TransformerFactoryConfigurationError, TransformerException {
+		List<List<String>> records = csvToList("AllergyConcernAct");
+		Document body = convertFileToDocument(INPUT_PATH + "C-CDA_R2-1_CCD.xml");
+		StringBuffer buff = new StringBuffer();
+		buff.append("//act/*");
+		for (int i = 1; i < records.size(); i++) {
+			buff.append("[not (self::" + records.get(i).get(0).replaceAll("\\[]", "") + ")]");
+		}
+		NodeList xPathDocument = (NodeList) XPATH.evaluate(buff.toString(), body, XPathConstants.NODESET);
+		convertNodeListToFile(xPathDocument, OUTPUT_PATH + "C-CDA_R2-1_CCD-unmapped.xml");
 	}
 
 }
