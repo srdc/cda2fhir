8cc5b0bd1569512f33d7ebef3ca9d296d2345b15
Afsin Ustundag <afsin@amida.com>
2019-04-11 08:34:16 -0400

    start problems sample testing

--- src/test/java/tr/com/srdc/cda2fhir/jolt/ProblemConcernActTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/jolt/ProblemConcernActTest.java
@@ -1,18 +1,24 @@
 package tr.com.srdc.cda2fhir.jolt;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Condition;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openhealthtools.mdht.uml.cda.EntryRelationship;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolPackage;
+import org.openhealthtools.mdht.uml.cda.consol.ContinuityOfCareDocument;
 import org.openhealthtools.mdht.uml.cda.consol.ProblemConcernAct;
 import org.openhealthtools.mdht.uml.cda.consol.ProblemObservation;
+import org.openhealthtools.mdht.uml.cda.consol.ProblemSection;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.openhealthtools.mdht.uml.hl7.datatypes.II;
 import org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship;
@@ -35,6 +41,8 @@ public class ProblemConcernActTest {
 	private static ResourceTransformerImpl rt;
 
 	private static final String OUTPUT_PATH = "src/test/resources/output/jolt/ProblemConcernAct/";
+
+	private static Consumer<Map<String, Object>> customJoltUpdate; // Hack for now
 
 	@BeforeClass
 	public static void init() {
@@ -105,7 +113,12 @@ public class ProblemConcernActTest {
 		} else {
 			for (int index = 0; index < conditions.size(); ++index) {
 				Condition condition = conditions.get(index);
-				joltUtil.verify(condition, joltConditions.get(index));
+				Map<String, Object> joltCondition = joltConditions.get(index);
+				if (customJoltUpdate != null) {
+					customJoltUpdate.accept(joltCondition);
+				}
+
+				joltUtil.verify(condition, joltCondition);
 			}
 		}
 	}
@@ -113,6 +126,21 @@ public class ProblemConcernActTest {
 	private static void runTest(ProblemConcernActGenerator generator, String caseName) throws Exception {
 		ProblemConcernAct pca = generator.generate(factories);
 		runTest(pca, caseName, generator);
+	}
+
+	private static void runSampleTest(String sourceName) throws Exception {
+		FileInputStream fis = new FileInputStream("src/test/resources/" + sourceName);
+		ContinuityOfCareDocument cda = (ContinuityOfCareDocument) CDAUtil.loadAs(fis,
+				ConsolPackage.eINSTANCE.getContinuityOfCareDocument());
+
+		ProblemSection section = cda.getProblemSection();
+
+		int index = 0;
+		for (ProblemConcernAct act : section.getConsolProblemConcerns()) {
+			String caseName = sourceName.substring(0, sourceName.length() - 4) + "_" + index;
+			runTest(act, caseName, null);
+			++index;
+		}
 	}
 
 	@Test
@@ -125,5 +153,140 @@ public class ProblemConcernActTest {
 	public void testFull() throws Exception {
 		ProblemConcernActGenerator generator = ProblemConcernActGenerator.getFullInstance();
 		runTest(generator, "fullCase");
+	}
+
+	@Test
+	public void testSample1() throws Exception {
+		runSampleTest("C-CDA_R2-1_CCD.xml");
+	}
+
+	@Test
+	public void testSample2() throws Exception {
+		runSampleTest("170.315_b1_toc_gold_sample2_v1.xml");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testSample3() throws Exception {
+		customJoltUpdate = r -> {
+			List<Object> codings = JoltUtil.findPathValue(r, "code.coding[]");
+			codings.forEach(coding -> {
+				Map<String, Object> codingAsMap = (Map<String, Object>) coding;
+				String code = (String) codingAsMap.get("code");
+				if ("346.1".equals(code)) {
+					codingAsMap.put("code", "346.10");
+				}
+			});
+		};
+		runSampleTest("Vitera_CCDA_SMART_Sample.xml");
+		customJoltUpdate = null;
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample1() throws Exception {
+		runSampleTest("Epic/DOC0001.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample2() throws Exception {
+		runSampleTest("Epic/DOC0001 2.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample3() throws Exception {
+		runSampleTest("Epic/DOC0001 3.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample4() throws Exception {
+		runSampleTest("Epic/DOC0001 4.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample5() throws Exception {
+		runSampleTest("Epic/DOC0001 5.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample6() throws Exception {
+		runSampleTest("Epic/DOC0001 6.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample7() throws Exception {
+		runSampleTest("Epic/DOC0001 7.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample8() throws Exception {
+		runSampleTest("Epic/DOC0001 8.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample9() throws Exception {
+		runSampleTest("Epic/DOC0001 9.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample10() throws Exception {
+		runSampleTest("Epic/DOC0001 10.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample11() throws Exception {
+		runSampleTest("Epic/DOC0001 11.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample12() throws Exception {
+		runSampleTest("Epic/DOC0001 12.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample13() throws Exception {
+		runSampleTest("Epic/DOC0001 13.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample14() throws Exception {
+		runSampleTest("Epic/DOC0001 14.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample15() throws Exception {
+		runSampleTest("Epic/DOC0001 15.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample16() throws Exception {
+		runSampleTest("Epic/HannahBanana_EpicCCD.xml");
+	}
+
+	@Ignore
+	@Test
+	public void testCernerSample1() throws Exception {
+		runSampleTest("Cerner/Person-RAKIA_TEST_DOC00001 (1).XML");
+	}
+
+	@Ignore
+	@Test
+	public void testCernerSample2() throws Exception {
+		runSampleTest("Cerner/Encounter-RAKIA_TEST_DOC00001.XML");
 	}
 }
