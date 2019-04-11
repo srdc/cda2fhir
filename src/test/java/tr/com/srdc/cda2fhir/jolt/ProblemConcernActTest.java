af7d585f8604b692611f3241eb27bd64668d7177
Afsin Ustundag <afsin@amida.com>
2019-04-11 07:10:47 -0400

    test cleaning

--- src/test/java/tr/com/srdc/cda2fhir/jolt/ProblemConcernActTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/jolt/ProblemConcernActTest.java
@@ -1,6 +1,7 @@
 package tr.com.srdc.cda2fhir.jolt;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +10,12 @@ import org.hl7.fhir.dstu3.model.Condition;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.EntryRelationship;
 import org.openhealthtools.mdht.uml.cda.consol.ProblemConcernAct;
+import org.openhealthtools.mdht.uml.cda.consol.ProblemObservation;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
+import org.openhealthtools.mdht.uml.hl7.datatypes.II;
+import org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship;
 
 import tr.com.srdc.cda2fhir.conf.Config;
 import tr.com.srdc.cda2fhir.testutil.CDAFactories;
@@ -18,8 +23,11 @@ import tr.com.srdc.cda2fhir.testutil.CDAUtilExtension;
 import tr.com.srdc.cda2fhir.testutil.JoltUtil;
 import tr.com.srdc.cda2fhir.testutil.generator.ProblemConcernActGenerator;
 import tr.com.srdc.cda2fhir.transform.ResourceTransformerImpl;
+import tr.com.srdc.cda2fhir.transform.ValueSetsTransformerImpl;
 import tr.com.srdc.cda2fhir.transform.entry.IEntryResult;
+import tr.com.srdc.cda2fhir.transform.util.IdentifierMapFactory;
 import tr.com.srdc.cda2fhir.transform.util.impl.BundleInfo;
+import tr.com.srdc.cda2fhir.transform.util.impl.IdentifierMap;
 import tr.com.srdc.cda2fhir.util.FHIRUtil;
 
 public class ProblemConcernActTest {
@@ -35,9 +43,37 @@ public class ProblemConcernActTest {
 		rt = new ResourceTransformerImpl();
 	}
 
-	private static void runTest(ProblemConcernActGenerator generator, String caseName) throws Exception {
-		ProblemConcernAct pca = generator.generate(factories);
+	private static void reorderActObservations(ProblemConcernAct act, List<Condition> conditions) {
+		ValueSetsTransformerImpl vst = new ValueSetsTransformerImpl();
+		IdentifierMap<Integer> orderMap = IdentifierMapFactory.resourcesToOrder(conditions);
+		List<ProblemObservation> observations = new ArrayList<>();
+		conditions.forEach(r -> observations.add(null));
+		act.getProblemObservations().forEach(r -> {
+			II ii = r.getIds().get(0);
+			String root = ii.getRoot();
+			String extension = ii.getExtension();
+			if (extension == null) {
+				Integer index = orderMap.get("Condition", root);
+				observations.set(index.intValue(), r);
+			} else {
+				String value = extension;
+				String system = vst.tOid2Url(root);
+				Integer index = orderMap.get("Condition", system, value);
+				observations.set(index.intValue(), r);
+			}
+		});
 
+		act.getEntryRelationships().clear();
+		observations.forEach(po -> {
+			EntryRelationship er = factories.base.createEntryRelationship();
+			act.getEntryRelationships().add(er);
+			er.setTypeCode(x_ActRelationshipEntryRelationship.SUBJ);
+			er.setObservation(po);
+		});
+	}
+
+	private static void runTest(ProblemConcernAct pca, String caseName, ProblemConcernActGenerator generator)
+			throws Exception {
 		Config.setGenerateNarrative(false);
 		Config.setGenerateDafProfileMetadata(false);
 
@@ -52,8 +88,12 @@ public class ProblemConcernActTest {
 					index == 0 ? "" : index, "json");
 			FHIRUtil.printJSON(condition, filepath);
 		}
-		generator.verify(bundle);
+		if (generator != null) {
+			generator.verify(bundle);
+		}
 
+		// CDAUtil reorders randomly, follow its order for easy comparison
+		reorderActObservations(pca, conditions);
 		File xmlFile = CDAUtilExtension.writeAsXML(pca, OUTPUT_PATH, caseName);
 
 		List<Object> joltResult = JoltUtil.findJoltResult(xmlFile, "ProblemConcernAct", caseName);
@@ -65,11 +105,14 @@ public class ProblemConcernActTest {
 		} else {
 			for (int index = 0; index < conditions.size(); ++index) {
 				Condition condition = conditions.get(index);
-				int joltIndex = condition.hasAsserter() ? 1 : 0; // TODO: fix, this is hack
-
-				joltUtil.verify(condition, joltConditions.get(joltIndex));
+				joltUtil.verify(condition, joltConditions.get(index));
 			}
 		}
+	}
+
+	private static void runTest(ProblemConcernActGenerator generator, String caseName) throws Exception {
+		ProblemConcernAct pca = generator.generate(factories);
+		runTest(pca, caseName, generator);
 	}
 
 	@Test
