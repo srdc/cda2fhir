437af840581380accb0ee9213ca49d9432909940
Matthew McCall <matt@mattmccall.com>
2019-04-11 14:10:26 -0700

    break out code walk into own func.

--- src/main/java/tr/com/srdc/cda2fhir/transform/util/impl/ReferenceInfo.java
+++ src/main/java/tr/com/srdc/cda2fhir/transform/util/impl/ReferenceInfo.java
@@ -13,6 +13,32 @@ import org.hl7.fhir.dstu3.model.StringType;
 public class ReferenceInfo {
 
 	/**
+	 * Walks a codeable concept and returns a string representation.
+	 *
+	 * @param entry codeable concept to analyze.
+	 * @return string of the codeable concept.
+	 */
+	private static String getStringFromConcept(CodeableConcept entry) {
+		if (entry != null) {
+			if (entry.hasText()) {
+				return entry.getText();
+			} else {
+				// if no text loop the displays and take the first one.
+				if (entry.getCoding() != null) {
+					if (entry.getCoding().size() > 0) {
+						for (Coding codeEntry : entry.getCoding()) {
+							if (codeEntry.getDisplay() != null) {
+								return codeEntry.getDisplay();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
 	 * Returns a string representation used for display assignment for references.
 	 *
 	 * @param resource to be referenced which will be searched for a display value.
@@ -25,34 +51,19 @@ public class ReferenceInfo {
 		// take coded object to get display value if possible.
 		if (resource.getNamedProperty("code") != null) {
 			if (!resource.getNamedProperty("code").getValues().isEmpty()) {
-
 				CodeableConcept code = (CodeableConcept) resource.getNamedProperty("code").getValues().get(0);
-				if (code != null) {
-					if (code.hasText()) {
-						return code.getText();
-					} else {
-						// if no text loop the displays and take the first one.
-						if (code.getCoding() != null) {
-							if (code.getCoding().size() > 0) {
-								for (Coding codeEntry : code.getCoding()) {
-									if (codeEntry.getDisplay() != null) {
-										return codeEntry.getDisplay();
-									}
-								}
-							}
-						}
-					}
+				String outputString = getStringFromConcept(code);
+				if (outputString != null) {
+					return outputString;
 				}
 			}
-
 		} else if (resource.getNamedProperty("vaccineCode") != null
 				&& !resource.getNamedProperty("vaccineCode").getValues().isEmpty()) {
 			CodeableConcept vaccineCode = (CodeableConcept) resource.getNamedProperty("vaccineCode").getValues().get(0);
-
-			if (vaccineCode != null && vaccineCode.hasText()) {
-				return vaccineCode.getText();
+			String outputString = getStringFromConcept(vaccineCode);
+			if (outputString != null) {
+				return outputString;
 			}
-
 		} else if (resource.getNamedProperty("name") != null
 				&& !resource.getNamedProperty("name").getValues().isEmpty()) {
 			Object nameObj = resource.getNamedProperty("name").getValues().get(0);
