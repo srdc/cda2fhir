f6dd87792b10c293c9e11965ec9de969fe7f82e3
Afsin Ustundag <afsin@amida.com>
2019-04-11 17:43:35 -0400

    sample test on entry level for procedures

--- src/test/java/tr/com/srdc/cda2fhir/jolt/ProcedureActivityProcedureTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/jolt/ProcedureActivityProcedureTest.java
@@ -1,14 +1,19 @@
 package tr.com.srdc.cda2fhir.jolt;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.Procedure;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolPackage;
+import org.openhealthtools.mdht.uml.cda.consol.ContinuityOfCareDocument;
 import org.openhealthtools.mdht.uml.cda.consol.ProcedureActivityProcedure;
+import org.openhealthtools.mdht.uml.cda.consol.ProceduresSection;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 
 import tr.com.srdc.cda2fhir.conf.Config;
@@ -66,9 +71,148 @@ public class ProcedureActivityProcedureTest {
 		runTest(pap, caseName, generator);
 	}
 
+	private static void runSampleTest(String sourceName) throws Exception {
+		FileInputStream fis = new FileInputStream("src/test/resources/" + sourceName);
+		ContinuityOfCareDocument cda = (ContinuityOfCareDocument) CDAUtil.loadAs(fis,
+				ConsolPackage.eINSTANCE.getContinuityOfCareDocument());
+
+		ProceduresSection section = cda.getProceduresSection();
+		if (section != null) {
+			int index = 0;
+			for (ProcedureActivityProcedure act : section.getConsolProcedureActivityProcedures()) {
+				String caseName = sourceName.substring(0, sourceName.length() - 4) + "_" + index;
+				runTest(act, caseName, null);
+				++index;
+			}
+		}
+	}
+
 	@Test
 	public void testDefault() throws Exception {
 		ProcedureActivityProcedureGenerator generator = ProcedureActivityProcedureGenerator.getDefaultInstance();
 		runTest(generator, "defaultCase");
+	}
+
+	@Test
+	public void testSample1() throws Exception {
+		runSampleTest("C-CDA_R2-1_CCD.xml");
+	}
+
+	@Test
+	public void testSample2() throws Exception {
+		runSampleTest("170.315_b1_toc_gold_sample2_v1.xml");
+	}
+
+	@Test
+	public void testSample3() throws Exception {
+		runSampleTest("Vitera_CCDA_SMART_Sample.xml");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample1() throws Exception {
+		runSampleTest("Epic/DOC0001.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample2() throws Exception {
+		runSampleTest("Epic/DOC0001 2.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample3() throws Exception {
+		runSampleTest("Epic/DOC0001 3.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample4() throws Exception {
+		runSampleTest("Epic/DOC0001 4.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample5() throws Exception {
+		runSampleTest("Epic/DOC0001 5.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample6() throws Exception {
+		runSampleTest("Epic/DOC0001 6.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample7() throws Exception {
+		runSampleTest("Epic/DOC0001 7.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample8() throws Exception {
+		runSampleTest("Epic/DOC0001 8.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample9() throws Exception {
+		runSampleTest("Epic/DOC0001 9.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample10() throws Exception {
+		runSampleTest("Epic/DOC0001 10.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample11() throws Exception {
+		runSampleTest("Epic/DOC0001 11.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample12() throws Exception {
+		runSampleTest("Epic/DOC0001 12.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample13() throws Exception {
+		runSampleTest("Epic/DOC0001 13.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample14() throws Exception {
+		runSampleTest("Epic/DOC0001 14.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample15() throws Exception {
+		runSampleTest("Epic/DOC0001 15.XML");
+	}
+
+	@Ignore
+	@Test
+	public void testEpicSample16() throws Exception {
+		runSampleTest("Epic/HannahBanana_EpicCCD.xml");
+	}
+
+	@Ignore
+	@Test
+	public void testCernerSample1() throws Exception {
+		runSampleTest("Cerner/Person-RAKIA_TEST_DOC00001 (1).XML");
+	}
+
+	@Ignore
+	@Test
+	public void testCernerSample2() throws Exception {
+		runSampleTest("Cerner/Encounter-RAKIA_TEST_DOC00001.XML");
 	}
 }
