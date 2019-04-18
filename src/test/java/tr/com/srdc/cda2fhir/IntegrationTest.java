096ce942a6cc6074f88f09a8b797db24862e77a8
steven <steven.staley@amida.com>
2019-04-18 09:28:29 -0400

    uncommenting out integration test

--- src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
+++ src/test/java/tr/com/srdc/cda2fhir/IntegrationTest.java
@@ -4,18 +4,29 @@ package tr.com.srdc.cda2fhir;
 import java.io.IOException;
 
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.dstu3.model.Bundle.BundleEntryResponseComponent;
 import org.hl7.fhir.dstu3.model.Bundle.BundleType;
+import org.hl7.fhir.dstu3.model.Device;
+import org.hl7.fhir.dstu3.model.DocumentReference;
 import org.hl7.fhir.dstu3.model.Identifier;
 import org.hl7.fhir.dstu3.model.Medication;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Practitioner;
+import org.hl7.fhir.dstu3.model.Provenance;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import tr.com.srdc.cda2fhir.testutil.BundleUtil;
 import tr.com.srdc.cda2fhir.transform.CCDTransformerImpl;
 import tr.com.srdc.cda2fhir.util.FHIRUtil;
 import tr.com.srdc.cda2fhir.util.IdGeneratorEnum;
@@ -43,9 +54,9 @@ public class IntegrationTest {
 		logger = LoggerFactory.getLogger(ValidatorImpl.class);
 	}
 
-//	@ClassRule
-//	public static DockerComposeRule docker = DockerComposeRule.builder().file("src/test/resources/docker-compose.yaml")
-//			.waitingForService("hapi", HealthChecks.toRespondOverHttp(8080, (port) -> port.inFormat(hapiURL))).build();
+	@ClassRule
+	public static DockerComposeRule docker = DockerComposeRule.builder().file("src/test/resources/docker-compose.yaml")
+			.waitingForService("hapi", HealthChecks.toRespondOverHttp(8080, (port) -> port.inFormat(hapiURL))).build();
 
 	@Test
 	public void rakiaIntegration() throws Exception {
@@ -62,33 +73,32 @@ public class IntegrationTest {
 //		// print pre-post bundle
 		FHIRUtil.printJSON(transactionBundle, "src/test/resources/output/rakia-4-17.json");
 
-		BundleUtil.findResources(transactionBundle, Medication.class, 13);
 		// Send transaction bundle to server.
-//		Bundle resp = client.transaction().withBundle(transactionBundle).execute();
-//
-//		for (BundleEntryComponent entry : resp.getEntry()) {
-//			BundleEntryResponseComponent entryResp = entry.getResponse();
-//
-//			Assert.assertEquals("201 Created", entryResp.getStatus());
-//		}
-//
-//		Bundle patientResults = (Bundle) client.search().forResource(Patient.class).prettyPrint().execute();
-//
-//		Bundle practitionerResults = (Bundle) client.search().forResource(Practitioner.class).prettyPrint().execute();
-//
-//		Bundle medicationResults = (Bundle) client.search().forResource(Medication.class).prettyPrint().execute();
-//
-//		Bundle provenanceResults = (Bundle) client.search().forResource(Provenance.class).prettyPrint().execute();
-//
-//		Bundle docRefresults = (Bundle) client.search().forResource(DocumentReference.class).prettyPrint().execute();
-//
-//		Bundle deviceResults = (Bundle) client.search().forResource(Device.class).prettyPrint().execute();
-//
-//		Assert.assertEquals(1, patientResults.getTotal());
-//		Assert.assertEquals(33, practitionerResults.getTotal());
-//		Assert.assertEquals(13, medicationResults.getTotal());
-//		Assert.assertEquals(1, provenanceResults.getTotal());
-//		Assert.assertEquals(1, docRefresults.getTotal());
-//		Assert.assertEquals(1, deviceResults.getTotal());
+		Bundle resp = client.transaction().withBundle(transactionBundle).execute();
+
+		for (BundleEntryComponent entry : resp.getEntry()) {
+			BundleEntryResponseComponent entryResp = entry.getResponse();
+
+			Assert.assertEquals("201 Created", entryResp.getStatus());
+		}
+
+		Bundle patientResults = (Bundle) client.search().forResource(Patient.class).prettyPrint().execute();
+
+		Bundle practitionerResults = (Bundle) client.search().forResource(Practitioner.class).prettyPrint().execute();
+
+		Bundle medicationResults = (Bundle) client.search().forResource(Medication.class).prettyPrint().execute();
+
+		Bundle provenanceResults = (Bundle) client.search().forResource(Provenance.class).prettyPrint().execute();
+
+		Bundle docRefresults = (Bundle) client.search().forResource(DocumentReference.class).prettyPrint().execute();
+
+		Bundle deviceResults = (Bundle) client.search().forResource(Device.class).prettyPrint().execute();
+
+		Assert.assertEquals(1, patientResults.getTotal());
+		Assert.assertEquals(33, practitionerResults.getTotal());
+		Assert.assertEquals(13, medicationResults.getTotal());
+		Assert.assertEquals(1, provenanceResults.getTotal());
+		Assert.assertEquals(1, docRefresults.getTotal());
+		Assert.assertEquals(1, deviceResults.getTotal());
 	}
 }
